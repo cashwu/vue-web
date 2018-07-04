9bbfe98f847dbeb6e43eea80e4ff64ff9e0b5b24
Cash Wu <cash@cashwu.com>
2018-07-04 21:33:12 +0800

    啟用一個 Vue Cli 並且 引用帶入專屬 API

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,21 @@
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+  created() {
+    const api =  `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/products`;
+    //"https://vue-course-api.hexschool.io/api/cashwu/products";
+    console.log(process.env.API_PATH, process.env.CUSTOM_PATH);
+    this.$http.get(api).then(response => {
+      console.log(response.data);
+    });
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
