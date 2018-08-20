ef76fbf614049bfaeb8d28ad8612abd1e874642d
Cash Wu <cash@cashwu.com>
2018-08-20 22:35:26 +0800

    create and edit product

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -1,85 +1,203 @@
 <template>
     <div>
         <div class="text-right mt-4">
-            <button class="btn btn-primary" @click="openModal">Create Product</button>
+            <button class="btn btn-primary" @click="openModal(true)">Create Product</button>
         </div>
         <table class="table mt-4">
             <thead>
-                <tr>
-                    <th width="120">Category</th>
-                    <th>Title</th>
-                    <th width="120">Origin Price</th>
-                    <th width="120">Price</th>
-                    <th width="100">IsEnable</th>
-                    <th width="80">Edit</th>
-                </tr>
+            <tr>
+                <th width="120">Category</th>
+                <th>Title</th>
+                <th width="120">Origin Price</th>
+                <th width="120">Price</th>
+                <th width="100">IsEnable</th>
+                <th width="80">Edit</th>
+            </tr>
             </thead>
             <tbody>
-                <tr v-for="(item) in products" :key="item.id" >
-                    <td>{{item.category}}</td>
-                    <td>{{item.title}}</td>
-                    <td class="text-right">{{item.origin_price}}</td>
-                    <td class="text-right">{{item.price}}</td>
-                    <td>
-                        <span v-if="item.is_enabled" class="text-success">Enable</span>
-                        <span v-else>Disable</span>
-                    </td>
-                    <td>
-                        <button class="btn btn-outline-primary btn-sm">
-                            Edit
-                        </button>   
-                    </td>
-                </tr>
+            <tr v-for="(item) in products" :key="item.id">
+                <td>{{item.category}}</td>
+                <td>{{item.title}}</td>
+                <td class="text-right">{{item.origin_price}}</td>
+                <td class="text-right">{{item.price}}</td>
+                <td>
+                    <span v-if="item.is_enabled" class="text-success">Enable</span>
+                    <span v-else>Disable</span>
+                </td>
+                <td>
+                    <button class="btn btn-outline-primary btn-sm" @click="openModal(false, item)">
+                        Edit
+                    </button>
+                </td>
+            </tr>
             </tbody>
         </table>
-        <div class="modal fade" id="productModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+
+        <div class="modal fade" id="productModal" tabindex="-1" role="dialog"
+             aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content border-0">
+                    <div class="modal-header bg-dark text-white">
+                        <h5 class="modal-title" id="exampleModalLabel">
+                            <span>新增產品</span>
+                        </h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <div class="form-group">
+                                    <label for="image">輸入圖片網址</label>
+                                    <input type="text" class="form-control" id="image"
+                                           v-model="tempProduct.imageUrl"
+                                           placeholder="請輸入圖片連結">
+                                </div>
+                                <div class="form-group">
+                                    <label for="customFile">或 上傳圖片
+                                        <i class="fas fa-spinner fa-spin"></i>
+                                    </label>
+                                    <input type="file" id="customFile" class="form-control"
+                                           ref="files">
+                                </div>
+                                <img img="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=828346ed697837ce808cae68d3ddc3cf&auto=format&fit=crop&w=1350&q=80"
+                                     class="img-fluid" :src="tempProduct.imageUrl" alt="">
+                            </div>
+                            <div class="col-sm-8">
+                                <div class="form-group">
+                                    <label for="title">標題</label>
+                                    <input type="text" class="form-control" id="title"
+                                           v-model="tempProduct.title"
+                                           placeholder="請輸入標題">
+                                </div>
+
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label for="category">分類</label>
+                                        <input type="text" class="form-control" id="category"
+                                               v-model="tempProduct.category"
+                                               placeholder="請輸入分類">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="price">單位</label>
+                                        <input type="unit" class="form-control" id="unit"
+                                               v-model="tempProduct.unit"
+                                               placeholder="請輸入單位">
+                                    </div>
+                                </div>
+
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label for="origin_price">原價</label>
+                                        <input type="number" class="form-control" id="origin_price"
+                                               v-model="tempProduct.origin_price"
+                                               placeholder="請輸入原價">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="price">售價</label>
+                                        <input type="number" class="form-control" id="price"
+                                               v-model="tempProduct.price"
+                                               placeholder="請輸入售價">
+                                    </div>
+                                </div>
+                                <hr>
+
+                                <div class="form-group">
+                                    <label for="description">產品描述</label>
+                                    <textarea type="text" class="form-control" id="description"
+                                              v-model="tempProduct.description"
+                                              placeholder="請輸入產品描述"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label for="content">說明內容</label>
+                                    <textarea type="text" class="form-control" id="content"
+                                              v-model="tempProduct.content"
+                                              placeholder="請輸入產品說明內容"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox"
+                                               v-model="tempProduct.is_enabled"
+                                               :true-value="1" :false-value="0"
+                                               id="is_enabled">
+                                        <label class="form-check-label" for="is_enabled">
+                                            是否啟用
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">取消</button>
+                        <button type="button" class="btn btn-primary" @click="updateProduct">確認</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script>
 
-import $ from 'jquery';
+    import $ from 'jquery'
 
-export default {
-  data() {
-    return {
-      products: []
-    };
-  },
-  methods: {
-    getProducts() {
-      const api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/admin/products`;
-      const vm = this;
-      console.log(process.env.API_PATH, process.env.CUSTOM_PATH);
-      this.$http.get(api).then(response => {
-        console.log(response.data);
-        vm.products = response.data.products;
-      });
-    },
-    openModal() {
-        $('#productModal').modal('show')
+    export default {
+        data () {
+            return {
+                products   : [],
+                tempProduct: {},
+                isNew      : false
+            }
+        },
+        methods: {
+            getProducts () {
+                const api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/admin/products`
+                const vm = this
+                console.log(process.env.API_PATH, process.env.CUSTOM_PATH)
+                this.$http.get(api).then(response => {
+                    console.log(response.data)
+                    vm.products = response.data.products
+                })
+            },
+            openModal (isNew, item) {
+
+                if (isNew) {
+                    this.tempProduct = {}
+                    this.isNew = true
+                } else {
+                    this.tempProduct = Object.assign({}, item)
+                    this.isNew = false
+                }
+
+                $('#productModal').modal('show')
+            },
+            updateProduct () {
+                let api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/admin/product`
+                let httpMethod = 'post'
+                const vm = this
+
+                if (!vm.isNew) {
+                    api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/admin/product/${vm.tempProduct.id}`
+                    httpMethod = 'put'
+                }
+
+                console.log(process.env.API_PATH, process.env.CUSTOM_PATH)
+                this.$http[httpMethod](api, {data: vm.tempProduct}).then(response => {
+                    console.log(response.data)
+                    if (response.data.success) {
+                        $('#productModal').modal('hide')
+                    } else {
+                        console.log('Create failed')
+                    }
+                    vm.getProducts()
+                })
+            }
+        },
+        created () {
+            this.getProducts()
+        }
     }
-  },
-  created() {
-      this.getProducts();
-  }
-};
 </script>
